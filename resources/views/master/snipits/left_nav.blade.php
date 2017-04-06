afb60b399c5f9d1bd70270395d039c81f43c02cb
Sean Alaback <sean.alaback@gmail.com>
2017-04-06 09:15:44 -0400

    Add Datasets to Navigation

--- resources/views/master/snipits/left_nav.blade.php
+++ resources/views/master/snipits/left_nav.blade.php
@@ -1,6 +1,6 @@
 <ul id="navigation">
     <li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
     <li><a href="{{route('properties.index')}}"><i class="fa fa-home"></i> <span>Properties</span></a></li>
-
+    <li><a href="{{route('dataset.index')}}"><i class="fa fa-home"></i> <span>Data Sets</span></a></li>
 </ul>
 <!--/ NAVIGATION Content -->
