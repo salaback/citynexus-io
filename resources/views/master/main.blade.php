91de684ce3b34e9b47db1ab60c61b595089ab5b8
Sean Alaback <sean.alaback@gmail.com>
2017-04-13 11:45:49 -0400

    Add favIcon

--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>CityNexus | @yield('title')</title>
-    <link rel="icon" type="image/ico" href="assets/images/favicon.ico" />
+    <link rel="icon" type="image/ico" href="/favicon.ico" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/assets/css/vendor/animsition.min.css">
