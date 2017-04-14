6793351677c79b11fa5727c78541deedd00b39bd
Sean Alaback <sean.alaback@gmail.com>
2017-04-14 13:09:35 -0400

    Add map to dataset

--- resources/views/dataset/show/profile.blade.php
+++ resources/views/dataset/show/profile.blade.php
@@ -11,6 +11,7 @@
             </div>
             <div class="boxs-body">
             @if($dataset->uploaders->count() > 0)
+                @php($uploaders = $dataset->uploaders()->orderBy('updated_at', "DEC")->paginate(5))
                     <table class="table m-b-0">
                         <thead>
                         <tr>
@@ -21,7 +22,7 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($dataset->uploaders as $uploader)
+                        @foreach($uploaders as $uploader)
 
                             <tr class="">
                                 <td>{{$uploader->name}}</td>
@@ -41,8 +42,49 @@
                         </div>
                     </div>
                 @endif
+                {{$uploaders->links()}}
             </div>
             <a href="{{route('uploader.create')}}?dataset_id={{$dataset->id}}" class="btn btn-raised btn-default btn-sm pull-right">Create New Uploader</a>
+        </section>
+    </div>
+
+    <div class="col-md-6">
+        <section class="boxs">
+            <div class="boxs-header dvd dvd-btm">
+                <h1 class="custom-font"><strong>Data Fields</strong></h1>
+            </div>
+            <div class="boxs-body">
+                @if(count($dataset->schema) > 0)
+                    <table class="table m-b-0">
+                        <thead>
+                        <tr>
+                            <th>Visible</th>
+                            <th>Field Name</th>
+                            <th>Key</th>
+                            <th>Type</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($dataset->schema as $item)
+                            <tr class="">
+                                <td>@if(isset($item['show']) && $item['show'] =='on') <i class="fa fa-check"></i> @endif</td>
+                                <td>{{$item['name']}}</td>
+                                <td>{{$item['key']}}</td>
+                                <td>{{$item['type']}}</td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                @else
+                    <div class="alert alert-info">
+                        <div class="alert-body">
+                            You don't have any uploaders yet! Create your first one.
+                            <a href="{{route('uploader.create')}}?dataset_id={{$dataset->id}}" class="btn btn-raised btn-primary">Create Uploader</a>
+                        </div>
+                    </div>
+                @endif
+            </div>
         </section>
     </div>
 @endsection
