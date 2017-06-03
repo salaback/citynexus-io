e467c4afd213160e1fb7e0be82b462282a0a9da9
Sean Alaback <sean.alaback@gmail.com>
2017-06-03 07:15:32 -0400

    Fix form errors

--- resources/views/master/_form_error.blade.php
+++ resources/views/master/_form_error.blade.php
@@ -1,7 +1,7 @@
 <script>
-@if (count($errors) > 0)
+@if(count($errors) > 0)
             @foreach ($errors->all() as $error)
-                alert('warning', {{ $error }})
+                alert('warning', '{{ $error }}');
             @endforeach
 @endif
 </script>
