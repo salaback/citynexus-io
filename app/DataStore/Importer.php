672335614b2d75929ef5ffdcc1f9d32e99ed3d84
Sean Alaback <sean.alaback@gmail.com>
2017-07-17 09:14:44 -0400

    set time limit

--- app/DataStore/Importer.php
+++ app/DataStore/Importer.php
@@ -116,7 +116,8 @@ class Importer
 
     private function stageData($data, $upload_id)
     {
-        ini_set('max_execution_time', 180);
+        set_time_limit(180);
+
         // chunk the array to groups of 100
         $chunks = array_chunk($data, 100);
         $files = [];
